a8c0356d3c456ed2980825d9115738db97d5034e
jeremyhorwich <jeremyahorwich@gmail.com>
2024-04-22 01:10:23 -0500

    Update board.py

--- board.py
+++ board.py
@@ -1,17 +1,33 @@
 import piece
 
 class Board:
-    state = [[],[],[],[],[],[],[],[],
-             [],[],[],[],[],[],[],[],
-             [],[],[],[],[],[],[],[],
-             [],[],[],[],[],[],[],[],
-             [],[],[],[],[],[],[],[],
-             [],[],[],[],[],[],[],[],
-             [],[],[],[],[],[],[],[],
-             [],[],[],[],[],[],[],[],]
+    state = [[[],[],[],[],[],[],[],[]],
+             [[],[],[],[],[],[],[],[]],
+             [[],[],[],[],[],[],[],[]],
+             [[],[],[],[],[],[],[],[]],
+             [[],[],[],[],[],[],[],[]],
+             [[],[],[],[],[],[],[],[]],
+             [[],[],[],[],[],[],[],[]],
+             [[],[],[],[],[],[],[],[]] ]
     
     def __init__(self, pos_data):
         pass
 
+
     def insert_piece(self, position, piece: piece):
         pass
+
+
+    def piece_at(self, position: str):
+        if len(position) != 2:
+            raise Exception("Position length ", len(position), " expected 2")
+        if not position[0].isalpha():
+            raise Exception("Position " + position + " in unexpected format.")
+        if not position[1].isdigit():
+            raise Exception("Position " + position + " in unexpected format.")
+        
+        #When playing as white, we want to call bottom up
+        column_index = ord(position[0]) - 97
+        row_index = 8 - int(position[1])
+        print(row_index, column_index)
+        return self.state[row_index][column_index]
