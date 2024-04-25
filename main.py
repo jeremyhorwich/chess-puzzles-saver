e33de52382c21039504a6821a18f9485b808b5fb
jeremyhorwich <jeremyahorwich@gmail.com>
2024-04-25 17:54:05 -0500

    Add saving puzzles to new file

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
-from puzzle import Puzzle
+from puzzle import Puzzle, save
 from board import Board
 import fen
 
-def read_boards_from(file: str):
+def create_new_puzzleset():
     pass
 
+def load_starting_menu():
+    print("1. Load Set")
+    print("2. Play Random Set")
+    print("3. Create New Set")
+    print("4. Import New Puzzles")
+    print("\n\n\n\n\n")
+    choice = input()
+    match choice:
+        case "4":
+            load_import_puzzle_menu()
+
+def load_import_puzzle_menu():
+    print("1. Many (from file)") 
+    print("2. One")
+    print("\n\n\n\n\n\n\n")
+    choice = input()
+    match choice:
+        case "2":
+            create_new_puzzle()
+
+def create_new_puzzle():
+    fen = input("Fen? ")
+    solution = input("Solution? ")
+    explanation = input("Explanation? ")
+    new_puzzle = Puzzle(fen, solution, explanation)
+    tags_raw = input("\nTags? ")
+    tags = tags_raw.split(", ")
+    for tag in tags:
+        save(new_puzzle, tag)
+
+
 if __name__ == "__main__": 
-    fen_raw = input("Fen? ")
-    usable_position_data = fen.load(fen_raw)
-    new_board = Board(usable_position_data[0])
-    print(new_board)
+    # fen_raw = input("Fen? ")
+
+    # usable_position_data = fen.load(fen_raw)
+    # new_board = Board(usable_position_data[0])
+    # print()
+    # print(new_board)
+    load_starting_menu()
--- puzzle.py
+++ puzzle.py
@@ -1,10 +1,18 @@
-class Puzzle:
-    position = None
-    solution = None
-    explanation = None
+import os
 
+class Puzzle:
     def __init__(self, fen: str, solution: str, explanation: str):
         self.fen = fen
         self.solution = solution
         self.explanation = explanation
-        
+
+def save(puzzle: Puzzle, filename: str):
+    PUZZLESET_DIRECTORY = "sets/"
+    filepath = os.path.join(PUZZLESET_DIRECTORY, filename + ".txt")
+    puzzleset = open(filepath,"a")
+
+    #TODO: Don't put in newline if file doesn't exist
+    export = puzzle.fen + "|" + puzzle.solution + "|" + puzzle.explanation
+
+    puzzleset.write("\n" + export)
+    puzzleset.close()
