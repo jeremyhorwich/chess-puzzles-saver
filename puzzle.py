82eec663a3be6dadf058ed6157a0be24b36583e2
jeremyhorwich <jeremyahorwich@gmail.com>
2024-04-26 00:28:57 -0500

    Update puzzle.py
    Loading puzzles

--- puzzle.py
+++ puzzle.py
@@ -1,18 +1,33 @@
 import os
 
+PUZZLESET_DIRECTORY = "sets/"
+
 class Puzzle:
     def __init__(self, fen: str, solution: str, explanation: str):
         self.fen = fen
         self.solution = solution
         self.explanation = explanation
 
+
 def save(puzzle: Puzzle, filename: str):
-    PUZZLESET_DIRECTORY = "sets/"
     filepath = os.path.join(PUZZLESET_DIRECTORY, filename + ".txt")
     puzzleset = open(filepath,"a")
 
     #TODO: Don't put in newline if file doesn't exist
+    #TODO: Eventually we swtich to JSON formats
+    #TODO: Loop through group of puzzles (so we don't open and close
+    #   repeatedly)
     export = puzzle.fen + "|" + puzzle.solution + "|" + puzzle.explanation
 
     puzzleset.write("\n" + export)
     puzzleset.close()
+
+
+def load_set(filename: str) -> list[Puzzle]:
+    filepath = os.path.join(PUZZLESET_DIRECTORY, filename + ".txt")
+    stored_puzzles = open(filepath,"r")
+    loaded = list()
+    for line in stored_puzzles:
+        puzzle_data = line.split("|")
+        loaded.append(Puzzle(puzzle_data[0],puzzle_data[1],puzzle_data[2]))
+    return loaded
