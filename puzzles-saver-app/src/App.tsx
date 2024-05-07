93c5d2445351454874ab23a98677906849fb4fe9
jeremyhorwich <71720955+jeremyhorwich@users.noreply.github.com>
2024-05-07 15:18:48 -0500

    Partially rework HTML into static JSX

--- puzzles-saver-app/src/App.tsx
+++ puzzles-saver-app/src/App.tsx
@@ -1,17 +1,60 @@
-import './App.css'
+import './styles.css'
 
 function App() {
   return (
-    <div><Test msg="test message"/></div>
+    <div className="puzzleViewer">
+      <TopBar/>
+      <Puzzle/>
+    </div>
   )
 }
 
-type TestProps = {
-    msg: string
+function TopBar() {
+  return (
+    <div className="topbar"></div>
+  )
 }
 
-export function Test(props: TestProps) {
-    return <h1>{props.msg}</h1>
+function Puzzle() {
+  return (
+    <div className="puzzle">
+      <Board/>
+      <PuzzleMeta/>
+    </div>
+  )
+}
+
+function Board() {
+  return (
+    <div className="board"></div>
+  )
+}
+
+
+function PuzzleMeta() {
+  const pgn = "1. e4 c5 2. c3 Nf6 3. e5 Nd5 4. Nf3 d6 5. Bb5+ Bd7 6. Bc4 Bc6 7. O-O e6 8. d4"
+  return (
+    <div className="puzzleMeta">
+      <Notation/>
+      <OtherOptions/>
+    </div>
+  )
+}
+
+type notation = {
+  pgn: String
+}
+
+function Notation(props: notation) {
+  return (
+    <div className="notation">{props.pgn}</div>
+  )
+}
+
+function OtherOptions() {
+  return (
+    <div className="otherOptions"></div>
+  )
 }
 
 export default App
