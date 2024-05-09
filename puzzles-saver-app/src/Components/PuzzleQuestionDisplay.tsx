6441bbeb6d17cb3737c2a357254dd67a9252fae7
jeremyhorwich <71720955+jeremyhorwich@users.noreply.github.com>
2024-05-09 15:46:11 -0500

    Partial refactor to stop whole component rerendering

--- puzzles-saver-app/src/Components/PuzzleQuestionDisplay.tsx
+++ puzzles-saver-app/src/Components/PuzzleQuestionDisplay.tsx
@@ -8,46 +8,67 @@ type puzzleQuestion = {
 }
 
 function PuzzleQuestionDisplay(props: puzzleQuestion) {
-    const [value, setValue] = useState<string | null>(null);
+    const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
+    let correctSelected = false;
 
     function handleClick(key: number) {
-        let msg;
-        msg = (key === 0) ? "Correct" : "Incorrect";
-        setValue(msg)
+        setSelectedAnswer(key)
+        correctSelected = (key === 0) ? true : false;
     }
 
+    return (
+        <div className="puzzleQuestionDisplay">
+            <ShuffledAnswers correctAnswer={props.correctAnswer} 
+                             wrongAnswers={props.wrongAnswers} 
+                             handleClick={handleClick} />
+            {selectedAnswer && <CorrectnessOfAnswer correct={correctSelected}/>}            
+        </div>
+    )
+  };
+
+type PuzzleAnswers = {
+    correctAnswer: string
+    wrongAnswers: Array<string>
+    handleClick: Function
+};
+
+function ShuffledAnswers(props: PuzzleAnswers) {
     let answerKeys = 0;
     const answers = props.wrongAnswers.map((answer) => {
         answerKeys++
         return ( 
-        <button key={answerKeys} onClick={() => handleClick(answerKeys)}>
+        <button key={answerKeys} onClick={() => props.handleClick(answerKeys)}>
             {answer}
         </button>
         )
     }
     );
 
-    //Shuffle the answers (Fisher-Yates algorithm)
     answers.push(
-        <button key={0} onClick={() => handleClick(0)}>
+        <button key={0} onClick={() => props.handleClick(0)}>
             {props.correctAnswer}
         </button>
     )
 
+    //Shuffle the answers (Fisher-Yates algorithm)
     let m = answers.length;
     while (m) {
         const i = Math.floor(Math.random() * m--);
         [answers[m],answers[i]] = [answers[i],answers[m]]
     };
 
-
     return (
-        <div className="puzzleQuestionDisplay">
-            {props.question}
-            {answers}
-            {value}
-        </div>
+        <div>{answers}</div>
     )
-  };
+};
 
-  export default PuzzleQuestionDisplay
+
+function CorrectnessOfAnswer(props: {correct: boolean}) {
+    let msg = props.correct ? "Correct!" : "Try Again"
+    return (
+        <div>{msg}</div>
+    )
+};
+
+
+export default PuzzleQuestionDisplay;
