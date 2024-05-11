1b059dd4595ff8f1c573d92583caebcb8ab479f6
jeremyhorwich <71720955+jeremyhorwich@users.noreply.github.com>
2024-05-11 15:03:35 -0500

    Add static border highlight to chessboard component
    Add static border highlight to chessboard component

--- puzzles-saver-app/src/Components/Chessboard.tsx
+++ puzzles-saver-app/src/Components/Chessboard.tsx
@@ -1,16 +1,32 @@
 import "./chessBoardStyles.css";
-
+import { useId } from "react"
 export default Chessboard
 
 function Chessboard(){
+    function handleClick() {
+        //Store the highlighted square as state
+        //Then when the board is clicked we match up the id of the square
+        //with the highlight below
+    }
+
     function Square(index: number) {
-    
+        const id=useId()
+        
         const shouldBeBlack = ((Math.floor(index / 8) % 2) + (index % 2)) % 2 === 0;
         const backgroundColor = shouldBeBlack ? "#ffffff" : "#000000";
-   
+        
+        let styles;
+
+        styles = {
+            backgroundColor: backgroundColor,
+            border: (index === 8) ? "4px solid #cccc95" : "1px solid #000000",
+            zIndex: (index === 8) ? 1 : 0
+        }
+
         return (
-            <div key={index} className="square" style={{ backgroundColor }} ></div>
+            <div key={id} className="square" style={styles} ></div>
         )
+
     }
     
     const squares = Array<JSX.Element>(64)
