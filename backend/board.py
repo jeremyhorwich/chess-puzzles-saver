c701af9cd2a4d6a48b3d8570ad223c682ad16ef4
jeremyhorwich <71720955+jeremyhorwich@users.noreply.github.com>
2024-06-01 15:20:24 -0500

    Update fen conversion logic to be API friendly

--- backend/board.py
+++ backend/board.py
@@ -1,5 +1,6 @@
-#TODO: Format error handling
-def _algebraic_to_coord(algebraic_position: str) -> tuple[int, int]:
+import json
+
+def algebraic_to_coord(algebraic_position: str) -> tuple[int, int]:
     column = ord(algebraic_position[0]) - 97
     row = 8 - int(algebraic_position[1])
     return row, column
@@ -7,8 +8,8 @@ def _algebraic_to_coord(algebraic_position: str) -> tuple[int, int]:
 #TODO: Format error handling
 #TODO: Change to convert to algebraic notation, then in Board convert
 #   to coordinates. We don't need to know coords outside of board
-def _get_piece_coordinates(raw: str) -> list[tuple]:
-    coordinates = list()
+def get_piece_coordinates(raw: str) -> json:
+    coordinates = dict()
     current_row = 0
     current_column = 0
     for char in raw:
@@ -19,11 +20,14 @@ def _get_piece_coordinates(raw: str) -> list[tuple]:
         if char.isdigit():
             current_column += int(char)
             continue
-        coordinates.append((current_row,current_column,char))
+        if char == " ":
+            break
+        coordinates[(current_row*8) + current_column] = char
         current_column += 1
-    return coordinates
+    print(coordinates)
+    return json.dumps(coordinates)
 
-def _get_turn(raw: str) -> str:
+def get_turn(raw: str) -> str:
     if raw == "w":
         return 0
     return 1
@@ -35,75 +39,8 @@ def _get_turn(raw: str) -> str:
 #Sample FEN (for reference):
 #8/5k2/3p4/1p1Pp2p/pP2Pp1P/P4P1K/8/8 b - - 99 50
 
-def _fen_to_fields(fen: str) -> list:
+def fen_to_fields(fen: str) -> list:
     fields = fen.split()
-    fields[0] = _get_piece_coordinates(fields[0])
-    fields[1] = _get_turn(fields[1])
+    fields[0] = get_piece_coordinates(fields[0])
+    fields[1] = get_turn(fields[1])
     return fields
-
-class Board: 
-    #TODO: Protect this from being able to update ANY attribute
-    def __init__(self, fen: str):
-        self.state = [["","","","","","","",""],
-                      ["","","","","","","",""],
-                      ["","","","","","","",""],
-                      ["","","","","","","",""],
-                      ["","","","","","","",""],
-                      ["","","","","","","",""],
-                      ["","","","","","","",""],
-                      ["","","","","","","",""]]
-        
-        unpacked_fen = _fen_to_fields(fen)
-
-        self.turn = unpacked_fen[1]
-        self.castling = unpacked_fen[2]
-        self.en_passant = unpacked_fen[3]
-        self.halfmoves = unpacked_fen[4]
-        self.fullmoves = unpacked_fen[5]
-        
-        self.insert_pieces(unpacked_fen[0])
-
-
-    def __str__(self):
-        printout = ""
-        if self.turn == 0:
-            printout += "White to move     "
-        else:
-            printout += "Black to move     "
-        printout += "Castling: " + self.castling + "     "
-        printout += "En Passant: " + self.en_passant + "\n"
-        printout += "Halfmoves: " + str(self.halfmoves) + "     "
-        printout += "Fullmoves: " + str(self.fullmoves) + "\n"
-
-        for row in range(0,8):
-            row_printout = ""
-            for position in range(0,8):
-                occupant = self.state[row][position]
-                if occupant == "":
-                    row_printout += "[ ]"
-                else:
-                    row_printout += "[" + occupant + "]"
-            printout += row_printout + "\n"
-        return printout
-
-
-    def insert_pieces(self, piece_coordinates: list):
-        for location_info in piece_coordinates:            
-            row,column = location_info[0], location_info[1]
-            piece = location_info[2]
-            self.state[row][column] = piece
-
-
-    def piece_at(self, algebraic_position: str):
-        row, column = _algebraic_to_coord(algebraic_position)
-        return self.state[row][column]
-    
-    
-    #TODO: Legality
-    def move_piece(self, origin: str, target: str):
-        origin_row, origin_column = _algebraic_to_coord(origin)
-        target_row, target_column = _algebraic_to_coord(target)
-        piece = self.state[origin_row][origin_column]
-        self.state[origin_row][origin_column] = ""
-        self.state[target_row][target_column] = piece
-        #NOTE: Here is where we would log the capture
