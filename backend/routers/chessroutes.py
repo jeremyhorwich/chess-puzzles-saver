4c0b24b96aa1b41158f3c4b5473a333f85c05c73
jeremyhorwich <71720955+jeremyhorwich@users.noreply.github.com>
2024-06-06 12:19:00 -0500

    Renaming chess routes

--- backend/routers/chessroutes.py
+++ backend/routers/chessroutes.py
@@ -1,18 +1,12 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
 from board import get_piece_coordinates, raw_move_to_san
+from chess import square
 
 router = APIRouter(prefix="/chess")
 
 @router.get("/utils/fen-to-pieces-coords")
 async def get_coords_from(fen: str):
     return get_piece_coordinates(fen)
-
-class MoveRequest(BaseModel):
-    fen: str
-    origin: int
-    target: int
-
 
 @router.get("/utils/move-indices-to-san")
 async def convert_indices_to_san(fen: str, origin: int, target: int):
