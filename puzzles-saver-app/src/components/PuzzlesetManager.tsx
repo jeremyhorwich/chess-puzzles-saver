9c617e8eb8bd1fcd17c0f419e1217d333891fd49
jeremyhorwich <71720955+jeremyhorwich@users.noreply.github.com>
2024-06-14 13:27:19 -0500

    Configure pre-fetch data

--- puzzles-saver-app/src/components/PuzzlesetManager.tsx
+++ puzzles-saver-app/src/components/PuzzlesetManager.tsx
@@ -4,29 +4,33 @@ import { PuzzlePlayer } from "./PuzzlePlayer";
 import useGetPuzzleset from "../hooks/fetches/useGetPuzzleset";
 import useGetPuzzle from "../hooks/fetches/useGetPuzzle";
 
+const startingPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
 type PuzzleSetManagerProps = {
     puzzlesetID: string
 }
 
 function PuzzlesetManager(props: PuzzleSetManagerProps) {
-    const [currentPuzzleIndex, setCurrentPuzzleIndex] = useState<number>(0);
-    const [currentPuzzle, setCurrentPuzzle] = useState<Puzzle>({fen: "", answer: ""})
-    const puzzleset = useRef<Puzzleset>({name: "", puzzles: [""]})
+    const [currentPuzzleIndex, setCurrentPuzzleIndex] = useState<number>(-1);
+    const puzzleObject = useRef<Puzzle>({fen: startingPosition, answer: "", title: "Loading Puzzle"});
+    const puzzleset = useRef<Puzzleset>({name: "", puzzles: [""]});
 
     useEffect(() => {
         useGetPuzzleset(props.puzzlesetID)
             .then((puzzlesetJSON) => {
-                puzzleset.current = puzzlesetJSON
+                puzzleset.current = puzzlesetJSON;
                 useGetPuzzle(puzzlesetJSON.puzzles[currentPuzzleIndex])
-                    .then((puzzleJSON) => {setCurrentPuzzle(puzzleJSON)})
+                    .then((puzzleJSON) => {puzzleObject.current = puzzleJSON});
             })
-    }, [])
+        
+        setCurrentPuzzleIndex(0);       //Force a rerender now that we have data
+    }, []);
 
     useEffect(() => {
         if (puzzleset.current.name === "") return;
         useGetPuzzle(puzzleset.current.puzzles[currentPuzzleIndex])
-            .then((puzzleJSON) => {setCurrentPuzzle(puzzleJSON)})
-    }, [currentPuzzleIndex])
+            .then((puzzleJSON) => {puzzleObject.current = puzzleJSON});
+    }, [currentPuzzleIndex]);
 
     function handleClick(direction: -1 | 1) {
         setCurrentPuzzleIndex(currentPuzzleIndex + direction);
@@ -34,14 +38,11 @@ function PuzzlesetManager(props: PuzzleSetManagerProps) {
 
     return (
         <div>
-            {currentPuzzle.fen}
-            {/* <PuzzlePlayer fen={currentPuzzle.fen} answer={currentPuzzle.answer}/>
+            <PuzzlePlayer {...puzzleObject.current}/>
             {currentPuzzleIndex > 0 && <button onClick={() => handleClick(-1)}>Button 1</button>}
-            {currentPuzzleIndex && <button onClick={() => handleClick(1)}>Button 2</button>} */}
+            {currentPuzzleIndex && <button onClick={() => handleClick(1)}>Button 2</button>}
         </div>
     )
 }
 
-export default PuzzlesetManager
-
-//Buttons for going to the next puzzle
+export default PuzzlesetManager;
