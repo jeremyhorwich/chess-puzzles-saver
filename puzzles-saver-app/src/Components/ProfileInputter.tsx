4ec5be02de237f1d02e7b6bada2e2e3185f1744c
jeremyhorwich <71720955+jeremyhorwich@users.noreply.github.com>
2024-06-15 14:37:36 -0500

    Create ProfileInputter.tsx

---
+++ puzzles-saver-app/src/Components/ProfileInputter.tsx
@@ -0,0 +1,10 @@
+function ProfileInputter() {
+    return (
+        <div>
+            <p>Get started</p>
+            <input type="text" placeholder="http://chess.com/members/yourprofile" required/>
+        </div>
+    )
+}
+
+export default ProfileInputter
