14cb2a98cf4ea5de7b928c7ff9c2522721220f22
jeremyhorwich <jeremyahorwich@gmail.com>
2024-06-24 12:11:00 -0500

    Add new functions to chesscom.py

--- backend/chesscom.py
+++ backend/chesscom.py
@@ -33,3 +33,29 @@ def get_player_monthly_archive(archive_url: HttpUrl = None,
 
     games = [MonthlyArchiveGame(**game) for game in j["games"]]
     return games
+
+
+def get_player_archives(profile: str) -> list[HttpUrl]:
+    url = f"https://api.chess.com/pub/player/{profile}/games/archives"
+    user_agent_header = {"user-agent": "chess-puzzles-saver/0.0.1"}
+    r = get(url, headers=user_agent_header)
+    j = r.json()
+    return j["archives"]
+
+
+def get_most_recent_games_from(profile: str, 
+                               number_to_find: int,
+                               **filters):
+    archives = get_player_archives(profile)
+    #TODO error handling - what if player has no games?
+    
+    games = []
+    for archive in reversed(archives):
+        next_most_recent_games = get_player_monthly_archive(archive)
+        next_most_recent_games.reverse()
+        games.extend(next_most_recent_games)
+        if len(games) >= number_to_find:
+            break
+    return games[0:number_to_find]
+
+print(get_most_recent_games_from("jeremyhorwich",2))
