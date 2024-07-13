d10498a8df41898203f6604f15bcc63a29c365c8
jeremyhorwich <71720955+jeremyhorwich@users.noreply.github.com>
2024-07-13 11:23:56 -0500

    Update main.tsx

--- puzzles-saver-app/src/main.tsx
+++ puzzles-saver-app/src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { PuzzlePlayer } from './Components/PuzzlePlayer'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <PuzzlePlayer fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" answer="e4" />
+    <App />
   </React.StrictMode>
 )
