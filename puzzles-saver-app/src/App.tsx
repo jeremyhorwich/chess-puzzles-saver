a7b1475c5584d7955d2677a08202729bc815c267
jeremyhorwich <71720955+jeremyhorwich@users.noreply.github.com>
2024-07-13 11:36:38 -0500

    update routing to reflect results removal

--- puzzles-saver-app/src/App.tsx
+++ puzzles-saver-app/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Landing from "./pages/Landing"
 import { Sets } from "./pages/Sets"
 import { PlayPuzzles } from "./pages/PlayPuzzles"
-import { Results } from "./pages/Results"
 import { Error } from "./pages/Error"
 
 
@@ -13,7 +12,6 @@ function App() {
                 <Route path="/" element={<Landing />}/>
                 <Route path="/sets" element={<Sets />}/>
                 <Route path="/playpuzzles" element={<PlayPuzzles />}/>
-                <Route path="/results" element={<Results />}/>
                 <Route path="/error" element={<Error />}/>
             </Routes>
         </Router>
